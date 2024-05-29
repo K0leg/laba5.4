892c1103191d72f575774022ae839f0d57d67930
K0leg <olegkovtun2005ua@gmail.com>
2024-05-29 22:09:22 +0300

    Add files via upload

---
+++ src/main/java/com/company/sixth.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class sixth {
+
+    public static void main(String[] args) {
+
+        int[][] array = new int[15][];
+
+        for (int i = 0; i < 5; i++) {
+            array[i] = new int[5];
+        }
+
+        for (int i = 4; i < 8; i++) {
+            array[i] = new int[8];
+        }
+
+        for (int i = 8; i < 12; i++) {
+            array[i] = new int[3];
+        }
+
+        for (int i = 12; i < 15; i++) {
+            array[i] = new int[9];
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 15);
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
